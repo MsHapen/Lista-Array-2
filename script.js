6b01e8adf1113faedeadcafa29ef44c23e8a361f
JaaJ <99226288+MsHapen@users.noreply.github.com>
2022-12-19 23:34:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+function novoElemento() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("inputItens").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("Eita, algo deu errado!");
+  } else {
+    document.getElementById("listaItens").appendChild(li);
+  }
+  document.getElementById("inputItens").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
